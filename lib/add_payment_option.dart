d3a841c963f53165bd29b6468b9bc9a4815dd8d2
Nensi Sheladiya <122796583+nensisheladiya@users.noreply.github.com>
2023-02-13 18:08:14 +0530

    add new file

---
+++ lib/add_payment_option.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddPayment extends StatefulWidget {
+  const AddPayment({Key? key}) : super(key: key);
+
+  @override
+  State<AddPayment> createState() => _AddPaymentState();
+}
+
+class _AddPaymentState extends State<AddPayment> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
