9b44ad773259860aa7ce887034d11c8d5df0354c
UtsavMoradiya <utsavmoradiya1666@gmail.com>
2023-02-13 09:56:40 +0530

    notification screen

--- lib/comment_screen.dart
+++ lib/comment_screen.dart
@@ -8,8 +8,48 @@ class CommentScreen extends StatefulWidget {
 }
 
 class _CommentScreenState extends State<CommentScreen> {
+  List<String> imagedata = [
+    "assets/images/imageone.png",
+    "assets/images/imagethree.png",
+    "assets/images/imagetwo.png",
+  ];
+  List<String> imagedataone = [
+    "assets/images/treeimage.png",
+    "assets/images/fellimage.png",
+    "assets/images/reposibleimage.png",
+  ];
+  List<String> textdata = [
+    "Catherine",
+    "Malcolm",
+    "Laura Rosell",
+  ];
+  List<String> datedata = [
+    "2019-03-12",
+    "2019-03-12",
+    "2019-03-12",
+  ];
+  List<String> textdataone = [
+    "When I ordered this book I did not\nrealize the author wasJanne linghurst,\nbut I soon realized it was her. ",
+    "I liked the story,  It seems a recurring\ntheme that a child must die in most of\nher stories. ",
+    "I bought this memoir based on the\nrecommendation from a friend. I felt like\nI was struggling with some things. ",
+  ];
+  List<String> textdatatwo = [
+    "The Hidden Life of Trees",
+    "And Then He Fell",
+    "Rethinking Possible: A Mem...",
+  ];
+  List<String> textdatathree = [
+    "Jane llinghurst",
+    "Kate Hewitt",
+    "Rebecca Faye Smith Galli",
+  ];
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    double width = size.width;
+    double height = size.height;
+    double text = MediaQuery.of(context).textScaleFactor;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -18,41 +58,66 @@ class _CommentScreenState extends State<CommentScreen> {
           Icons.arrow_back,
           color: Colors.black,
         ),
+        centerTitle: true,
         title: const Text(
           "Comment",
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              height: 220,
-              width: 405,
-              color: Colors.blue,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.asset("assets/images/Avatar.png"),
-                  Row(
-                    children: const [Text("")],
-                  )
-                ],
-              ),
+      body: SafeArea(
+        top: true,
+        bottom: true,
+        child: ListView.builder(
+          itemCount: imagedata.length,
+          itemBuilder: (context, index) => ListTile(
+            trailing: Image.asset("assets/images/hartimage.png"),
+            leading: Image.asset(imagedata[index]),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  textdata[index],
+                  style: TextStyle(color: Color(0xFF8E8E93)),
+                ),
+                Text(
+                  datedata[index],
+                  style: TextStyle(color: Color(0xFFC7C7CC)),
+                ),
+                SizedBox(
+                  height: height / 43,
+                ),
+                Text(textdataone[index]),
+                Container(
+                  height: height / 10,
+                  width: width / 1,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF1F2F3),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: height / 65, left: width / 20),
+                        child: Image.asset(imagedataone[index]),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(textdatatwo[index]),
+                          Text(
+                            textdatathree[index],
+                            style: TextStyle(color: Color(0xFFC7C7CC)),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Container(
-              height: 220,
-              width: 410,
-              color: Colors.blue,
-            ),
-            Container(
-              height: 220,
-              width: 410,
-              color: Colors.blue,
-            ),
-          ],
+          ),
         ),
       ),
     );
