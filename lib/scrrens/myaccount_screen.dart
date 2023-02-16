8ca00272043fa6e1322889525b80aa12aacbe78e
UtsavMoradiya <utsavmoradiya1666@gmail.com>
2023-02-16 17:21:28 +0530

    my accountscreen

--- lib/scrrens/myaccount_screen.dart
+++ lib/scrrens/myaccount_screen.dart
@@ -21,6 +21,12 @@ class _MyaccountScreenState extends State<MyaccountScreen> {
     "Daughters of War",
     "A Family Affair",
   ];
+  List<String> dollardata = [
+    "-＄5.50 ",
+    "-＄6.50",
+    "+＄80.00",
+    "-＄9.50",
+  ];
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -29,6 +35,7 @@ class _MyaccountScreenState extends State<MyaccountScreen> {
     double text = MediaQuery.of(context).textScaleFactor;
 
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: const Color(0XFF2EBBC3),
         elevation: 0,
@@ -42,71 +49,101 @@ class _MyaccountScreenState extends State<MyaccountScreen> {
       body: SafeArea(
         top: true,
         bottom: true,
-        child: Stack(
+        child: Column(
           children: [
-            Container(
-              height: height / 2.7,
-              width: double.infinity,
-              color: Colors.yellow,
-            ),
-            Container(
-              height: height / 3.5,
-              width: double.infinity,
-              color: Color(0XFF2EBBC3),
-              child: Column(
-                children: [
-                  Text(
-                    "Balance",
-                    style: TextStyle(color: Colors.white, fontSize: text * 13),
-                  ),
-                  Text(
-                    "65.00＄",
-                    style: TextStyle(color: Colors.white, fontSize: text * 40),
-                  ),
-                  SizedBox(
-                    height: height / 20,
-                  ),
-                  Container(
-                    height: height / 20,
-                    width: width / 2,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        "Deposit",
-                        style: TextStyle(
-                          color: Color(0xFF2EBBC3),
+            Stack(
+              children: [
+                Container(
+                  height: height / 2.7,
+                  width: double.infinity,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: height / 3.5,
+                  width: double.infinity,
+                  color: const Color(0XFF2EBBC3),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Balance",
+                        style: TextStyle(color: Colors.white, fontSize: text * 13),
+                      ),
+                      Text(
+                        "65.00＄",
+                        style: TextStyle(color: Colors.white, fontSize: text * 40),
+                      ),
+                      SizedBox(
+                        height: height / 20,
+                      ),
+                      Container(
+                        height: height / 20,
+                        width: width / 2,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "Deposit",
+                            style: TextStyle(
+                              color: Color(0xFF2EBBC3),
+                            ),
+                          ),
                         ),
                       ),
+                      SizedBox(
+                        height: height / 20,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: height / 4,
+                    left: width / 10,
+                    right: width / 10,
+                  ),
+                  child: Container(
+                    height: height / 10,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset("assets/images/bank card.png"),
+                      ],
                     ),
                   ),
-                  SizedBox(
-                    height: height / 20,
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: height / 4,
-                left: width / 10,
-                right: width / 10,
-              ),
-              child: Container(
-                height: height / 10,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
+            Row(
+              children: [
+                SizedBox(
+                  width: width / 14,
                 ),
-                child: Row(
-                  children: [
-                    Image.asset("assets/images/bank card.png"),
-                  ],
+                const Text(
+                  "Recent Transactions",
+                  style: TextStyle(
+                    color: Color(0XFFC7C7CC),
+                  ),
                 ),
-              ),
+                SizedBox(
+                  width: width / 2.6,
+                ),
+                const Text(
+                  "view all",
+                  style: TextStyle(
+                    color: Color(0XFFC7C7CC),
+                  ),
+                ),
+                const Icon(
+                  Icons.navigate_next,
+                  color: Color(0XFFC7C7CC),
+                )
+              ],
             )
           ],
         ),
