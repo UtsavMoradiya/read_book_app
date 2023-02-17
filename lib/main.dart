52ea42234ad867d725c507dc7c4cdda5564749d1
UtsavMoradiya <utsavmoradiya1666@gmail.com>
2023-02-17 11:38:24 +0530

    my account screen creat

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:read_book_app/scrrens/book_instruction_screen.dart';
+import 'package:read_book_app/scrrens/myaccount_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: BookInstructionScreen(),
+      home: MyaccountScreen(),
     );
   }
 }
